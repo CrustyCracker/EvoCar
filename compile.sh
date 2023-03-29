b7956fbcf7595ecd9d31e096fba5d2f19d0047b6
Jakub Mateusz Marcowski <01158831@pw.edu.pl>
2023-03-29 19:02:00 +0200

    Move OS specific lib files to a respective folder

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-clang++-14 -std=c++17 src/main.cpp -o main_clang++ -I include -L lib -l sfml-system -l sfml-window -l sfml-graphics -l sfml-audio -l sfml-network -l box2d -Wl,-rpath ./lib
-g++-12 -std=c++17 src/main.cpp -o main_g++ -I include -L lib -l sfml-system -l sfml-window -l sfml-graphics -l sfml-audio -l sfml-network -l box2d -Wl,-rpath ./lib
+clang++-14 -std=c++17 src/main.cpp -o main_clang++ -I include -L lib/linux -l sfml-system -l sfml-window -l sfml-graphics -l sfml-audio -l sfml-network -l box2d -Wl,-rpath ./lib/linux
+g++-12 -std=c++17 src/main.cpp -o main_g++ -I include -L lib/linux -l sfml-system -l sfml-window -l sfml-graphics -l sfml-audio -l sfml-network -l box2d -Wl,-rpath ./lib/linux
