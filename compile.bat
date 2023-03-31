ce777c07ad142e8ae652d3958f8499fe40ba8b42
Jakub Mateusz Marcowski <01158831@pw.edu.pl>
2023-03-31 15:51:34 +0200

    Change the structure of building scripts to be more unified

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 del build /F /Q
 mkdir build
 xcopy /s lib\windows build\
-g++ -std=c++17 src/main.cpp -o build/main_g++ -I include -L lib/windows -l sfml-system -l sfml-window -l sfml-graphics -l sfml-audio -l sfml-network -l box2d -Wl,-rpath lib/windows
+g++ -std=c++17 src/main.cpp -o build/main -I include -L lib/windows -l sfml-system -l sfml-window -l sfml-graphics -l sfml-audio -l sfml-network -l box2d
--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
-clang++-14 -std=c++17 src/main.cpp -o main_clang++ -I include -L lib/linux -l sfml-system -l sfml-window -l sfml-graphics -l sfml-audio -l sfml-network -l box2d -Wl,-rpath ./lib/linux
-g++-12 -std=c++17 src/main.cpp -o main_g++ -I include -L lib/linux -l sfml-system -l sfml-window -l sfml-graphics -l sfml-audio -l sfml-network -l box2d -Wl,-rpath ./lib/linux
+#!/bin/bash -li
+rm -rf build
+mkdir build
+g++ -std=c++17 src/main.cpp -o build/main -I include -L lib/linux -l sfml-system -l sfml-window -l sfml-graphics -l sfml-audio -l sfml-network -l box2d -Wl,-rpath ./lib/linux
