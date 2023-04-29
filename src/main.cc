93cca421f7d7bed6593ed6466a0299e27ec3986c
mkrakows <awpodkawpdok@o2.pl>
2023-04-29 16:24:03 +0200

    fastFix

--- src/main.cc
+++ src/main.cc
@@ -6,7 +6,7 @@
 #include "../include/imgui/imgui-SFML.h"
 #include "../include/imgui/imgui.h"
 
-b2World world(b2Vec2(0, GRAVITIATIONAL_ACCELERATION));
+b2World world(b2Vec2(0, Config::GRAVITIATIONAL_ACCELERATION));
 // A structure with all we need to render a box
 struct Box {
     float width;
