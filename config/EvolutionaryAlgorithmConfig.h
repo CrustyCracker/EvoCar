fa8a78446d7f0f107daa394b379d61c5913a72e5
mkrakows <matkrakowski1@gmail.com>
2023-05-18 11:27:22 +0200

    ok stałe

--- config/EvolutionaryAlgorithmConfig.h
+++ config/EvolutionaryAlgorithmConfig.h
@@ -15,22 +15,22 @@ class EvolutionaryAlgorithmConfig {
     static constexpr float MIN_BODY_LENGTH = 1.0f;
     static constexpr float MAX_BODY_LENGTH = 10.0f;
     static constexpr float MUTATION_RATE_BODY_LENGTHS = 0.1f;
-    static constexpr float MUTATION_FACTOR_BODY_LENGTHS = 0.1f;
+    static constexpr float MUTATION_FACTOR_BODY_LENGTHS = 0.5f;
     
-    static constexpr float MIN_BODY_DENSITY = 0.1f;
-    static constexpr float MAX_BODY_DENSITY = 10.0f;
-    static constexpr float MUTATION_RATE_BODY_DENSITY = 0.05f;
-    static constexpr float MUTATION_FACTOR_BODY_DENSITY = 0.1f;
+    static constexpr float MIN_BODY_DENSITY = 10.0f;
+    static constexpr float MAX_BODY_DENSITY = 1000.0f;
+    static constexpr float MUTATION_RATE_BODY_DENSITY = 0.2f;
+    static constexpr float MUTATION_FACTOR_BODY_DENSITY = 20.0f;
 
-    static constexpr float MIN_WHEEL_RADIUS = 0.1f;
-    static constexpr float MAX_WHEEL_RADIUS = 1.0f;
+    static constexpr float MIN_WHEEL_RADIUS = 10.0f;
+    static constexpr float MAX_WHEEL_RADIUS = 250.0f;
     static constexpr float MUTATION_RATE_WHEEL_RADIUS = 0.2f;
-    static constexpr float MUTATION_FACTOR_WHEEL_RADIUS = 0.1f;
+    static constexpr float MUTATION_FACTOR_WHEEL_RADIUS = 20.0f;
 
-    static constexpr float MIN_WHEEL_DENSITY = 0.1f;
-    static constexpr float MAX_WHEEL_DENSITY = 10.0f;
+    static constexpr float MIN_WHEEL_DENSITY = 10.0f;
+    static constexpr float MAX_WHEEL_DENSITY = 1000.0f;
     static constexpr float MUTATION_RATE_WHEEL_DENSITY = 0.1f;
-    static constexpr float MUTATION_FACTOR_WHEEL_DENSITY = 0.1f;
+    static constexpr float MUTATION_FACTOR_WHEEL_DENSITY = 20.0f;
 
     static constexpr float TOURNAMENT_SIZE = 3; // Has to be equal or greater than POPULATION_SIZE
 };
