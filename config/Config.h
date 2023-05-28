d1074b4c63b05451141582367a62c343f16518b2
mkrakows <matkrakowski1@gmail.com>
2023-05-28 10:00:28 +0200

    com correction

--- config/Config.h
+++ config/Config.h
@@ -22,7 +22,7 @@ class Config {
     // Draw debug geometry
     static constexpr bool DEBUG = true;
 
-    // Psychics
+    // Physics
     static constexpr double GRAVITIATIONAL_ACCELERATION = -9.81f;
     static constexpr float FRICTION = 0.3f;
     static constexpr double PI = 3.14159265358979323846;
