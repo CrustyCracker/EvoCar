02595a940e27c6ad6b7f9b5f65a6ee24bbde2b53
mkrakows <matkrakowski1@gmail.com>
2023-06-06 20:07:59 +0200

    added briefs

--- src/EvolutionaryAlgorithm.h
+++ src/EvolutionaryAlgorithm.h
@@ -32,18 +32,43 @@ class EvolutionaryAlgorithm {
     unsigned long int populationSize_;
     std::vector<Chromosome> population_;
     bool saveToFile_ = false;
+    /**
+     * @brief performs tournament selection on the population
+     * @return the winner of the tournament
+     */
     Chromosome tournament();
+    /**
+     * @brief initializes the population
+     */
     void initializePopulation();
 
    public:
+    /**
+     * @brief Construct a new Evolutionary Algorithm:: Evolutionary Algorithm object
+     * 
+     * @param populationSize  The size of the population
+     * @param saveToFile    Whether to save the population to a file
+     */
     explicit EvolutionaryAlgorithm(int populationSize, bool saveToFile = false);
     std::vector<Chromosome> getPopulation() { return population_; }
+    /**
+     * @brief mutates the population
+     */
     void mutate();
+    /**
+     * @brief performs tournament selection on the population
+     */
     void tournamentSelection();
+    /**
+     * @brief steps the algorithm to the next generation
+     */
     void nextGeneration();
     void setFitness(int index, float fitness) { population_[index].fitness = fitness; }
     int getGeneration() const { return generation_; }
     int getPopulationSize() const { return populationSize_; }
+    /**
+     * @brief exports the population to a json file
+     */
     int exportPopulation();
 };
 
