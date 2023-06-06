97431fc7708a5e4f9af4741dea41239d9addbda1
Jakub Mateusz Marcowski <01158831@pw.edu.pl>
2023-06-06 20:36:27 +0200

    Add Render.h's and UserInput.h's docblocks

--- src/Render.h
+++ src/Render.h
@@ -16,16 +16,54 @@
 #include "Shape.h"
 #include "Utility.h"
 
+/**
+ * @brief Function for rendering a circle.
+ *
+ * @param w SFML's RenderWindow.
+ * @param circle Pointer to the circle.
+ */
 void renderCircle(sf::RenderWindow &w, Circle *circle);
 
+/**
+ * @brief Function for rendering a circle's debug information.
+ *
+ * @param w SFML's RenderWindow.
+ * @param circle Pointer to the circle.
+ */
 void renderCircleDebug(sf::RenderWindow &w, Circle *circle);
 
+/**
+ * @brief Function for rendering a polygon.
+ *
+ * @param w SFML's RenderWindow.
+ * @param polygon Pointer to the polygon.
+ */
 void renderPolygon(sf::RenderWindow &w, Polygon *polygon);
 
+/**
+ * @brief Function for rendering a polygon's debug information.
+ *
+ * @param w SFML's RenderWindow.
+ * @param polygon Pointer to the polygon.
+ */
 void renderPolygonDebug(sf::RenderWindow &w, Polygon *polygon);
 
+/**
+ * @brief Function for rendering a car.
+ *
+ * @param w SFML's RenderWindow.
+ * @param car Car to be rendered.
+ */
 void renderCar(sf::RenderWindow &w, Car car);
 
+/**
+ * @brief Main render function.
+ *
+ * @param w SFML's RenderWindow.
+ * @param BGs Vector of background sprites.
+ * @param ground Vector of ground polygons.
+ * @param cars Vector of cars.
+ */
 void render(sf::RenderWindow &w, const std::vector<sf::Sprite> &BGs, std::vector<Polygon> &ground,
             std::vector<Car> &cars);
 
--- src/UserInput.h
+++ src/UserInput.h
@@ -12,9 +12,27 @@
 #include "imgui-SFML.h"
 #include "SFML/Graphics.hpp"
 
+/**
+ * @brief Function for handling user inputs.
+ *
+ * @param w SFML's RenderWindow.
+ * @param paused Whether the simulation is paused (bool).
+ * @param pause_check Whether the `paused` was turned on (bool).
+ * @param next_gen Whether the program should generate the next generation (bool).
+ * @param nxt_g_check Whether the `next_gen` was turned on (bool).
+ * @param focus Whether the window is in focus (bool).
+ */
 void handleUserInput(sf::RenderWindow &w, bool &paused, bool &pause_check, bool &next_gen,
                      bool &nxt_g_check, bool &focus);
 
+/**
+ * @brief Function for handling SFML events.
+ *
+ * @param w SFML's RenderWindow.
+ * @param pause_check Whether the `paused` was turned on (bool).
+ * @param nxt_g_check Whether the `next_gen` was turned on (bool).
+ * @param focus Whether the window is in focus (bool).
+ */
 void handleEvents(sf::RenderWindow &w, bool &pause_check, bool &nxt_g_check, bool &focus);
 
 #endif
